811f3e7545b21d2b1b7bacd6f5e2f1040251f844
rocco <rocco@hackerstribe.com>
2017-12-29 03:44:03 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const NetcatServer = require('netcat').server
+const {fork} = require('child_process')
+
+console.log('Started!')
+
+function forkWorker () {
+  return fork('./worker.js', {
+    stdio: [
+      'pipe',
+      'pipe', // Pipe child's stdout to parent
+      'pipe', // child's stderr to parent
+      'ipc'
+    ]
+  })
+}
+
+let nc = new NetcatServer()
+let workers = {}
+
+nc.k().port(2389).listen().on('connection', function(socket){
+  console.log(`${socket.id} connected`)
+  let worker = workers[socket.id] = forkWorker()
+  socket.pipe(worker.stdin)
+  worker.stdout.pipe(socket)
+}).on('clientClose', function(socket){
+  console.log(`${socket.id} disconnected`)
+  workers[socket.id].kill()
+})
---
+++ worker.js
@@ -0,0 +1,15 @@
+const repl = require('repl')
+
+console.log('worker started')
+
+let replServer = repl.start('> ')
+
+replServer.on('exit', () => {
+  console.log('Received "exit" event from repl!')
+  process.exit()
+})
+
+replServer.on('reset', function(){
+  console.log('context reset')
+})
+
