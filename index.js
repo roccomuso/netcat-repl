23e03633a3c673861ccd7fc8134f83fd2b27d45d
rocco <rocco@hackerstribe.com>
2017-12-31 03:55:47 +0100

    improve comments and api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const NetcatServer = require('netcat').server
 const {fork} = require('child_process')
+function noop(){}
 
 console.log('Started!')
 
@@ -17,15 +18,18 @@ function forkWorker () {
 let nc = new NetcatServer()
 let workers = {}
 
-exports.listen = function (port) {
+
+module.exports = function ({verbose, port}) {
+  if (!verbose) console.log = noop
+  port = port || process.env.PORT
 
   nc.k().port(port).listen().on('connection', function(socket){
-    console.log(`${socket.id} connected`)
+    console.log(`${socket.remoteAddress}:${socket.remotePort} (${socket.id}) - connected`)
     let worker = workers[socket.id] = forkWorker()
     socket.pipe(worker.stdin)
     worker.stdout.pipe(socket)
   }).on('clientClose', function(socket){
-    console.log(`${socket.id} disconnected`)
+    console.log(`${socket.remoteAddress}:${socket.remotePort} (${socket.id}) - disconnected`)
     workers[socket.id].kill()
   })
 
